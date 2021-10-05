93c117423bf58ff7d96c8162e4e8fbbc942a70a2
Luis <lmendoza@labsxd.com>
2021-10-05 14:52:58 -0300

    last change in turnos online datatable

--- src/app/pages/online-turn/online-turn.component.ts
+++ src/app/pages/online-turn/online-turn.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
+import {MatTableDataSource} from '@angular/material/table';
 import { Shiefts } from 'src/app/models/shiefts';
 import { TurnModel } from 'src/app/models/turn.model';
 import { OnlineTurnService } from 'src/app/services/online-turn.service';
@@ -19,8 +20,49 @@ export class OnlineTurnComponent implements OnInit {
   turnList : TurnModel[];
   shieftList : Shiefts[];
 
+  columns = [
+    {
+      columnDef: 'speciality',
+      header: 'Especialista',
+      cell: (element: Shiefts) => `${element.speciality}`
+    },
+    {
+      columnDef: 'nameChild',
+      header: 'Nombre del Paciente',
+      cell: (element: Shiefts) => `${element.nameChild}`
+    },
+    {
+      columnDef: 'ageChild',
+      header: 'Edad del Paciente',
+      cell: (element: Shiefts) => `${element.ageChild}`
+    },
+    {
+      columnDef: 'nameFather',
+      header: 'Nombre del Padre',
+      cell: (element: Shiefts) => `${element.nameFather}`
+    },
+    {
+      columnDef: 'emailfather',
+      header: 'Email Contacto',
+      cell: (element: Shiefts) => `${element.emailfather}`
+    },
+    {
+      columnDef: 'dateselect',
+      header: 'Dia del turno',
+      cell: (element: Shiefts) => `${element.dateselect}`
+    },
+    {
+      columnDef: 'hourselect',
+      header: 'Hora del turno',
+      cell: (element: Shiefts) => `${element.hourselect}`
+    }
+  ];
+  displayedColumns = this.columns.map(c => c.columnDef);
+  dataSource: MatTableDataSource<Shiefts>;
+
   constructor(public dialog : MatDialog,
-              public service : OnlineTurnService) { }
+              public service : OnlineTurnService) { 
+              }
 
   ngOnInit() {
     let data = this.service.getConectListApp();
@@ -32,7 +74,7 @@ export class OnlineTurnComponent implements OnInit {
           this.turnList.push(it as TurnModel);
       });
     });
-
+    
     let resers = this.service.getConectListShieftAgen();
     resers.snapshotChanges().subscribe(shieft =>{
         this.shieftList = [];
@@ -43,10 +85,12 @@ export class OnlineTurnComponent implements OnInit {
                 let vv = value;
                 vv["$key"] = key;
                 vv["speciality"] = namespeciality;
+                vv["dateselect"] = new Date(vv.dateselect).toLocaleDateString("en-US")
                 this.shieftList.push(vv as Shiefts);
             }
             );
-        })
+        });
+        this.dataSource = new MatTableDataSource(this.shieftList);
     });
   }
 
@@ -69,5 +113,10 @@ export class OnlineTurnComponent implements OnInit {
     if(confirm('¿Estas seguro de Eliminar este Dato?') == true){
        this.service.deleteDataProducts(item.$key);
     }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
